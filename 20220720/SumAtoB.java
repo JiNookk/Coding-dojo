758bbef52f4133028c56497e5042bd52adb333bf
jingwook <qnfdkf153@gmail.com>
2022-07-20 09:48:34 +0900

    Math.min, max를 이용한 풀이

--- 20220720/SumAtoB.java
+++ 20220720/SumAtoB.java
@@ -2,21 +2,15 @@ class Solution {
     public long solution(int a, int b) {
         long answer = 0;
 
-        int forLength = Math.abs(a - b);
+        int min = Math.min(a,b);
+        int max = Math.max(a,b);
 
-        if (forLength == 0) {
-            return a;
-        }
+        int forLength = max-min;
 
-        for (int i = 0; i <= forLength; i += 1) {
-            answer += a;
+        for (int i = 0; i <= forLength; i += 1){
+            answer += min;
 
-            if (a < b) {
-                a += 1;
-                continue;
-            }
-
-            a -= 1;
+            min += 1;
         }
 
         return answer;
