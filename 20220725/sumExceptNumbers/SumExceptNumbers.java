166a7a78da7ecffcabe90c2ee78ebc53dfad0e6b
jingwook <qnfdkf153@gmail.com>
2022-07-25 09:29:15 +0900

    마지막 줄 개행 추가 및 코드 포맷팅

--- 20220725/sumExceptNumbers/SumExceptNumbers.java
+++ 20220725/sumExceptNumbers/SumExceptNumbers.java
@@ -1,19 +1,19 @@
 class Solution {
     public int solution(int[] numbers) {
 
-        int [] zeroToNine = new int[]{0,1,2,3,4,5,6,7,8,9};
+        int[] zeroToNine = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         int sum = 0;
 
-        for (int num:zeroToNine){
+        for (int num : zeroToNine) {
             sum += num;
         }
 
         int answer = 0;
 
-        for (int num2:numbers){
-
-            sum -= num2;
+        for (int num2 : numbers) {
+            if (num2 > 0 && num2 < 10)
+                sum -= num2;
         }
 
         answer = sum;
