2aef6e428bfee6fe0f0e8e23352e9c126e1aeb71
jingwook <qnfdkf153@gmail.com>
2022-07-29 10:56:12 +0900

    불필요한 주석 삭제

--- 20220729/Add2DArray/Add2DArray.java
+++ 20220729/Add2DArray/Add2DArray.java
@@ -4,8 +4,8 @@ class Solution {
 
         int[][] temp = new int[arr1.length][arr1[1].length];
 
-        for (int i = 0; i < arr1.length; i += 1) { //arr배열의 n번째 배열요소
-            for (int j = 0; j < arr1[i].length; j += 1) {    //arr배열의 배열의 n번째 요소
+        for (int i = 0; i < arr1.length; i += 1) {
+            for (int j = 0; j < arr1[i].length; j += 1) {
                 temp[i][j] = arr1[i][j] + arr2[i][j];
             }
         }
