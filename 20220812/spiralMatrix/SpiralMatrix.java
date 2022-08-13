b7b7a348176fc78fbd76e9a7a9ca101469da7d2f
jingwook <qnfdkf153@gmail.com>
2022-08-13 15:40:14 +0900

    배열의 길이가 음수인 케이스 해결

--- 20220812/spiralMatrix/SpiralMatrix.java
+++ 20220812/spiralMatrix/SpiralMatrix.java
@@ -14,15 +14,24 @@ public class SpiralMatrix {
     }
 
     private void mySolution(int arrayLength) {
-        int[][] spiral = new int[arrayLength][arrayLength];
-        int length = arrayLength;
+        int length = Math.abs(arrayLength);
+        int[][] spiral = new int[length][length];
+
+        boolean isLengthPlus = true;
+        if (arrayLength < 0){
+            isLengthPlus = false;
+        }
         boolean isLength0 = false;
+        if (arrayLength == 0) {
+            isLength0 = true;
+        }
+
         int i = 0;
         int j = -1;
         int element = 0;
         int direction = 1;
 
-        while (!isLength0) {
+        while (!isLength0 && isLengthPlus) {
             for (int index = 0; index < length; index += 1) {
                 j += direction;
 
@@ -49,6 +58,41 @@ public class SpiralMatrix {
                 element += 1;
             }
 
+            direction *= -1;
+        }
+
+        if (!isLengthPlus){
+            int temp = i;
+            i = j;
+            j = temp;
+        }
+
+        while (!isLength0 && !isLengthPlus) {
+            for (int index = 0; index < length; index += 1) {
+                i += direction;
+
+                spiral[i][j] = element;
+
+                element += 1;
+            }
+
+            length -= 1;
+
+            if (length == 0) {
+                print(Math.abs(arrayLength),spiral);
+
+                isLength0= true;
+
+                continue;
+            }
+
+            for (int index = 0; index < length; index += 1) {
+                j += direction;
+
+                spiral[i][j] = element;
+
+                element += 1;
+            }
             direction *= -1;
         }
     }
