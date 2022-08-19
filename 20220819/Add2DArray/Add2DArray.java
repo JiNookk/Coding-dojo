55d9cb59680801f67879d7a1ce13e3452be1f6b5
jingwook <qnfdkf153@gmail.com>
2022-08-19 23:33:32 +0900

    20220819-Add2DArray

---
+++ 20220819/Add2DArray/Add2DArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {};
+
+        int[][] temp = new int[arr1.length][arr1[1].length];
+
+        for (int i = 0; i < arr1.length; i += 1) {
+            for (int j = 0; j < arr1[i].length; j += 1) {
+
+                temp[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        answer = temp;
+
+        return answer;
+    }
+}
