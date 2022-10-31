68e7357cbf83dcd9170fedbe3d7e8107da40d9cd
jingwook <qnfdkf153@gmail.com>
2022-10-31 22:25:30 +0900

    20221031-NQueen

---
+++ 20221031/NQueen/src/main/java/Queen.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queen {
+    private final int size;
+    protected List<Integer> coordinate = new ArrayList<>();
+
+    public Queen(int size) {
+        this.size = size;
+    }
+
+
+    public boolean[][] putOnBoard(boolean[][] chessBoard) {
+
+
+
+        return chessBoard;
+    }
+}
